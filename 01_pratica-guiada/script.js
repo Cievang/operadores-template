d483633f1036100857d08fed12dfdbb34c57f8dd
Cinthya Evangelista <cievang8401@gmail.com>
2023-02-01 22:34:40 -0300

    Exercícios de operações resolvidos

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,69 @@
-//CODIGO AQUI
+// PRÁTICA GUIADA 1
+
+//operador soma
+/*
+let saldo 
+console.log (`Valor incial do saldo: ${saldo}`)
+
+saldo = 15
+saldo = saldo + 22.40
+console.log (`valor após a primeira operação: ${saldo}`)
+
+// operador subtração
+
+saldo = saldo - 12.34
+console.log (`Saldo após segunda operação: ${saldo}`)
+
+// operador divisão
+
+saldo = saldo / 3
+console.log (`Saldo após divdir o dinheiro com os irmãos: ${saldo}`)
+
+// operador multiplicação
+
+saldo = saldo * 3.42
+console.log (`Saldo após investimento ${saldo}`)
+
+// operador de resto
+
+saldo = saldo % 3
+console.log(`Saldo que restou após repartir: ${saldo}`)
+
+// PRÁTICA GUIADA 2 
+*/
+/*
+const resultado1 = 5===5
+console.log('resutaldo 1',resultado1)
+
+const resultado2 = 5!==5
+console.log ('resultado 2', resultado2)
+
+const resultado3 = 5 !== '5'
+console.log ('resultado 3',resultado3)
+
+const resultado4 = '5' === 'cinco'
+console.log ('resultado 4', resultado4)
+
+const resultado5 = typeof 5 === typeof 20
+console.log('resultado 5',resultado5)
+
+const resultado6 = typeof '5' === typeof 'cinco'
+console.log ('resultado 6', resultado6)
+
+
+const primeiroNumero = 15
+const segundoNumero = 24
+
+const compara1 = primeiroNumero === segundoNumero
+console.log('O primeiro número é igual ao seungo?', compara1)
+
+const compara2 = primeiroNumero <= segundoNumero
+console.log (` o primeiro número é maior ou igual ao segundo número? ${compara2}`)
+
+const compara3 = primeiroNumero > segundoNumero
+console.log (' o primeiro número é maior que o segundo número?', compara3)
+
+const compara4 = primeiroNumero < segundoNumero
+console.log('o primeiro número é menor que o segundo numero?', compara4)
+*/
+
